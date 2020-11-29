a82d9d29cacb28b813479a17965e18c2c3f123c4
GoodGoodStudy20 <549687570@qq.com>
2020-11-30 00:41:56 +0800

    feat:热卖排行样式

---
+++ src/views/Home/Recommend/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="recommend">
+    <div class="recommend-container">
+      <ul class="today-recommend">
+        <li class="recommend-clock">
+          <div class="recommend-timer">
+            <a href="">
+              <img src="./images/clock.png" />
+            </a>
+            <h3>今日推荐</h3>
+          </div>
+        </li>
+        <li class="banner">
+          <a href=""><img src="./images/today01.png" /></a>
+        </li>
+        <li class="banner">
+          <a href=""><img src="./images/today02.png" /></a>
+        </li>
+        <li class="banner">
+          <a href=""><img src="./images/today03.png" /></a>
+        </li>
+        <li class="banner">
+          <a href=""><img src="./images/today04.png" /></a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Recommend",
+};
+</script>
+
+<style lang="less" scoped>
+.recommend-container {
+  width: 1200px;
+  margin: 0 auto;
+}
+.today-recommend {
+  height: 165px;
+  background-color: #eaeaea;
+  margin: 10px 0;
+  display: flex;
+}
+.recommend-clock {
+  width: 16.67%;
+  background-color: #5c5251;
+  color: #fff;
+  font-size: 18px;
+  text-align: center;
+}
+.recommend-timer {
+  padding: 30px 0;
+  h3 {
+    margin: 9px 0;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 30.06px;
+    color: #fff;
+  }
+}
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,17 +2,23 @@
   <div>
       <SortNav />
       <HomeContainer />
+      <Recommend />
+      <SellRank />
   </div>
 </template>
 
 <script>
 import SortNav from "../../components/SortNav"
 import HomeContainer from "./HomeContainer"
+import Recommend from "./Recommend"
+import SellRank from "./SellRank"
 export default {
   name: 'Home',
   components:{
       SortNav,
-      HomeContainer
+      HomeContainer,
+      Recommend,
+      SellRank
   }
 }
 </script>
